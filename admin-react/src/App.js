d6dd22cf1e664faba6e4afd3a48a71b52606e7c3
Zihan Wang <ziw224@lehigh.edu>
2023-11-04 23:50:30 -0400

    update App.js

--- admin-react/src/App.js
+++ admin-react/src/App.js
@@ -1,8 +1,22 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login"
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      admin
+      <Router>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home/>} />
+            <Route path="login" element={<Login/>} />
+            <Route path="users">
+              {/* <Route index element={<List/>}> */}
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
