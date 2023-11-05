d5c0bfaa4bf7378af2fc6b0777160ab08c81b4c9
Zihan Wang <ziw224@lehigh.edu>
2023-11-04 23:50:12 -0400

    adding home page

---
+++ admin-react/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../../components/sidebar/Sidebar"
+import "./home.scss";
+
+export const Home = () => {
+  return (
+    <div className="home">
+        <Sidebar/>
+    </div>
+  )
+};
+
+export default Home;
